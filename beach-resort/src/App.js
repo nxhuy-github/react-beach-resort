600f0fb8144a59c84b5342a817b92f33b8209a14
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-24 10:14:33 +0200

    use React Context...

--- beach-resort/src/App.js
+++ beach-resort/src/App.js
@@ -6,7 +6,7 @@ import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
 
-import {Route, Switch, Link} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 
 import Navbar from './components/Navbar'
 
