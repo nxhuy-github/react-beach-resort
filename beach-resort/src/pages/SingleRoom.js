62969de9724a1ea389e7fd99a900ee0c8f00ac28
NGUYEN XUAN HUY p1419771 <23189942+nxhuy-github@users.noreply.github.com>
2019-09-24 12:31:46 +0200

    complete SingleRoom

--- beach-resort/src/pages/SingleRoom.js
+++ beach-resort/src/pages/SingleRoom.js
@@ -26,9 +26,10 @@ export default class SingleRoom extends Component {
       )
     }
     const {name, description, capacity, size, price, extras, breakfast, pets, images} = room
+    const [mainImg, ...defaultImg] = images
     return (
       <>
-        <StyledHero img={images[0]}>
+        <StyledHero img={mainImg}>
           <Banner title={`${name} room`}>
             <Link to='/rooms' className="btn-primary">
               back to rooms
@@ -37,10 +38,32 @@ export default class SingleRoom extends Component {
         </StyledHero>
         <section className="single-room">
           <div className="single-room-images">
-            {images.map((img, idx) => {
+            {defaultImg.map((img, idx) => {
               return <img key={idx} src={img} alt={name}/>
             })}
           </div>
+          <div className="single-room-info">
+            <article className="desc">
+              <h3>details</h3>
+              <p>{description}</p>
+            </article>
+            <article className="info">
+              <h3>info</h3>
+              <h6>price : ${price}</h6>
+              <h6>size : ${size} SQFT</h6>
+              <h6>max capacity : {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
+              <h6>{pets ? "pets allowed" : "no pets allowed"}</h6>
+              <h6>{breakfast && "free breakfast included"}</h6>
+            </article>
+          </div>
+        </section>
+        <section className="room-extras">
+          <h6>extras</h6>
+          <ul className="extras">
+            {extras.map((item, idx) => {
+              return <li key={idx}>- {item}</li>
+            })}
+          </ul>
         </section>
       </>
     )
